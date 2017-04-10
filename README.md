8621483cfda381d0d4929df16f596fab55040ce8
opeyemi.onikute <opeyemi.onikute@konga.com>
2017-04-10 16:43:07 +0100

    Updated README and minor code refactor

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Meet Ejo, He's a Yoruba snake who eats only eba and fufu. Let's build him together.
 
 <h3>Game Info</h3>
-The game currently conists of: 
+The game currently consists of:
 - One Snake eating eba. 
 - A scoreboard
 - Health bar: Essentially you'd have to eat yourself three times to die. But the bushes at the edge are poisonous so they drain all of Ejo's health.
@@ -17,21 +17,21 @@ The game was built on Python 2.78, but any latter version works. The required fr
 - Move all the images into the same folder as the main.py file.
 - Open the main.py file with any editor of your choosing and run the game. 
 
-You could also create a [virtualenv](https://www.google.com.ng/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjFi8XbrM_NAhWrCcAKHfXvCCgQFggcMAA&url=http%3A%2F%2Fdocs.python-guide.org%2Fen%2Flatest%2Fdev%2Fvirtualenvs%2F&usg=AFQjCNEvupNSRAVxfumkI5JFoxABd0GHhQ&sig2=HZIahDpW2zapyX1ZGp8C_g), with Python 2.78, then install Pygame and Pyganim in the environment to be safe. 
+You could also create a <a href="https://www.google.com.ng/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjFi8XbrM_NAhWrCcAKHfXvCCgQFggcMAA&url=http%3A%2F%2Fdocs.python-guide.org%2Fen%2Flatest%2Fdev%2Fvirtualenvs%2F&usg=AFQjCNEvupNSRAVxfumkI5JFoxABd0GHhQ&sig2=HZIahDpW2zapyX1ZGp8C_g">virtualenv</a> with Python 2.78, then install Pygame and Pyganim in the environment to be safe.
 
 <h3>Features to be added</h3>
 <h4>Soundtrack</h4>
 We've been looking for a proper soundtrack, which is ideally a fast-paced Fuji beat. If you have any, submit a pull request while re-working the code and adding sound if you know how to. If you don't submit a pull request with the link to the sound regardless.
 
 <h4>Storyline</h4>
-I'd like to have a storyline, which would ideally be an animation that plays when the user first plays the game. Send any ideas or write-ups to me through [Twitter](http://www.twitter.com/ope__o), or [my e-mail](opeyemionikute@yahoo.com). We'd find a way to incorporate the storyline into the game afterwards when we do the animation. 
+I'd like to have a storyline, which would ideally be an animation that plays when the user first plays the game. Send any ideas or write-ups to me through <a href="http://www.twitter.com/ope__o">Twitter</a>, or <a href="mailto:opeyemionikute@yahoo.com">my e-mail</a>. We'd find a way to incorporate the storyline into the game afterwards when we do the animation.
 For example, "There is farmine in Abeokuta, and Ejo goes to Ibadan to gather all the Eba and fufu to take it back to his hometown. He has to also get tomatoes and grind pepper on his way back. But the citizens are afraid and want to kill him so he has to pass through the gutter. Ejo -- The Yoruba Hero."
 
 <h4>Bonus Level</h4>
 I've been dwelling on the idea of a bonus level or stage, maybe there'd be a buffet of Eba and Amala and Ejo gets to eat not only for the village, but for himself. It'll be a chance for the player to replenish health and reduced Ejo's length to ease game difficulty.
 
 <h4>High Score</h4>
-I just havent gotten around to doing it yet.
+I just haven't gotten around to doing it yet.
 
 That's pretty much it. Let's help make Ejo. I'd update this readme as the need arises.
 
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import health
 
 pygame.init()
 
-#Setting the display width and height
+# Setting the display width and height
 display_width =  800
 display_height = 600
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Ejo')
 
 
-#colours
+# colours
 white = (255,255,255)
 black = (0,0,0)
 grey = (220,220,220)
@@ -25,11 +25,11 @@ light_green = (0, 240, 0)
 yellow = (255, 255, 128)
 light_yellow = (255, 255, 228)
 
-#loading the animations
+# loading the animations
 gameOverAnim = pyganim.PygAnimation([('game_over2.png', 150),('game_over3.png', 100)])
 bonusAnim = pyganim.PygAnimation([('bonus_text1.png', 150),('bonus_text2.png', 100)])
 
-#loading the images
+# loading the images
 icon = pygame.image.load('snakeHead.png')
 img = pygame.image.load('snake.png')
 eba = pygame.image.load('eba2.png')
@@ -40,17 +40,17 @@ game_over_face = pygame.image.load('face005.png')
 skin = pygame.image.load('skin.png')
 bgdside = pygame.image.load('bgd_side.png')
 
-#setting the game icon to the snake head 
+# setting the game icon to the snake head
 pygame.display.set_icon(icon)
 
-#initializing the timing functionality and some standard variables
+# initializing the timing functionality and some standard variables
 clock = pygame.time.Clock()
 AppleThickness = 30
 block_size = 20
 FPS = 30
 direction = 'right'
 
-#Loading the fonts to be used
+# Loading the fonts to be used
 extra_smallfont = pygame.font.SysFont('comicsansms', 15)
 smallfont = pygame.font.SysFont('comicsansms', 20)
 small2font = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 40)
@@ -59,7 +59,7 @@ medfontButton = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.t
 medfontButton2 = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 80)
 largefont = pygame.font.SysFont('comicsansms', 80)
 
-#rendering the font for the copyright display at the bottom.
+# rendering the font for the copyright display at the bottom.
 sign = extra_smallfont.render('©2015.' , True, white)
 
 def message_to_screen(msg, color, y_displace=0, x_displace=0, size = "small", font = None, fontSize = None, blink=False):
@@ -99,11 +99,20 @@ def pause():
         pygame.display.update()
         clock.tick(5)
 
+
 def score_update(score):
+    """
+    :param score:
+    :return:
+    """
     text = smallfont.render('Score: ' + str(score), True, white)
     gameDisplay.blit(text, [display_width - 765,20])
-    
+
+
 def game_controls():
+    """
+    :return:
+    """
     gcont  = True
 
     while gcont:
@@ -137,7 +146,7 @@ def game_controls():
         text_to_button('Play', yellow, light_yellow, 250,455,80,40, size = 'small', action = 'play')
 
             
-##            text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
+        # text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
         
         text_to_button('Quit', red, light_red, 450,455,80,40 ,  size = 'small', action = 'quit')
         pygame.display.update()
@@ -145,16 +154,33 @@ def game_controls():
 
 
 def randAppleGen():
+    """
+    :return:
+    """
     randAppleX = round(random.randrange(40, display_width-AppleThickness-40))#/10)*10     #Eating with the Apple and regenerating a new apple somewhere else
     randAppleY = round(random.randrange(30, display_height-AppleThickness-30))#/10)*10
 
     return randAppleX, randAppleY
  
 def text_to_button(msg, color, inactive_color, buttonx, buttony, buttonwidth, buttonheight, size = None, action = None, blink = False):
+    """
+    :param msg:
+    :param color:
+    :param inactive_color:
+    :param buttonx:
+    :param buttony:
+    :param buttonwidth:
+    :param buttonheight:
+    :param size:
+    :param action:
+    :param blink:
+    :return:
+    """
+
     textSurf, textRect = text_objects_button(msg, color, size)
     textSize = textSurf.get_width()
     
-##    textRect.center = ((buttonx+(buttonwidth/2)), buttony+(buttonheight/2))
+    # textRect.center = ((buttonx+(buttonwidth/2)), buttony+(buttonheight/2))
     cur = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     gameDisplay.blit(textSurf, (buttonx, buttony))
@@ -173,9 +199,8 @@ def text_to_button(msg, color, inactive_color, buttonx, buttony, buttonwidth, bu
                 gameLoop()
             if action == 'main':
                 game_intro()
-    
-    
-##    pygame.display.update()
+
+        # pygame.display.update()
 
 
 def game_intro():
@@ -255,11 +280,18 @@ def Snake(block_size, snakeList):
     gameDisplay.blit(head,  (snakeList[-1][0], snakeList[-1][1]))
     for XnY in snakeList[:-1]:
         gameDisplay.blit(skin,  (XnY[0], XnY[1]))
-##        pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
+       # pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
     
-        
 
 def text_objects(text, color, size = None,  font = None, fontSize = None):
+    """
+    :param text:
+    :param color:
+    :param size:
+    :param font:
+    :param fontSize:
+    :return:
+    """
     if size == 'small':
         textSurface = smallfont.render(text, True, color)
     elif size == 'medium':
@@ -272,7 +304,14 @@ def text_objects(text, color, size = None,  font = None, fontSize = None):
         
     return textSurface, textSurface.get_rect()
 
-def text_objects_button(text, color, size = None):
+
+def text_objects_button(text, color, size=None):
+    """
+    :param text:
+    :param color:
+    :param size:
+    :return:
+    """
     if size == 'small':
         textSurface = small2font.render(text, True, color)
     elif size == 'medium':
@@ -281,12 +320,15 @@ def text_objects_button(text, color, size = None):
         textSurface = medfontButton2.render(text, True, color)
     elif size == 'large':
         textSurface = largefont.render(text, True, color)
-    
-        
+
     return textSurface, textSurface.get_rect()
 
-def health_bars(snake_health):
 
+def health_bars(snake_health):
+    """
+    :param snake_health:
+    :return:
+    """
     if snake_health > 75:
         snake_health_color = green
     elif snake_health > 50:
@@ -298,8 +340,6 @@ def health_bars(snake_health):
     pygame.draw.rect(gameDisplay, black , (display_width-131, 25, 92, 22))
     pygame.draw.rect(gameDisplay, white , (display_width-130, 26, 90, 20))
     pygame.draw.rect(gameDisplay, snake_health_color , (display_width-130, 26, snake_health, 20))
-
-
 
 
 def gameLoop():
@@ -338,8 +378,8 @@ def gameLoop():
             gameDisplay.blit(bgdside, (0,0))
             gameDisplay.blit(sign, [display_width/2 - 30,display_height-20])
             health_bars(snake_health)
-##            pygame.time.set_timer(Text1, 10)
-##            pygame.time.set_timer(Text2, 90)
+           # pygame.time.set_timer(Text1, 10)
+           # pygame.time.set_timer(Text2, 90)
             
             
             gameOverAnim.blit(gameDisplay, (263, 352))
@@ -365,13 +405,13 @@ def gameLoop():
             text_to_button('Play Again', black, light_yellow, 62,455,80,40, size = 'small', action = 'play')
             text_to_button('Play Again', yellow, light_yellow, 60,455,80,40, size = 'small', action = 'play')
             
-##            text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
+           # text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
             text_to_button('Quit', black, black, 622,455,80,40 ,  size = 'small', action = 'quit')
             text_to_button('Quit', red, light_red, 620,455,80,40 ,  size = 'small', action = 'quit')
 
-##            button('Play Again', 150,400,100,40, black, light_green, action = 'play')
-##            button('Controls', 350,400,100,40, black, light_green, action = 'controls')
-##            button('Quit', 570,400,80,40 , black, light_green, action = 'quit')
+           # button('Play Again', 150,400,100,40, black, light_green, action = 'play')
+           # button('Controls', 350,400,100,40, black, light_green, action = 'controls')
+           # button('Quit', 570,400,80,40 , black, light_green, action = 'quit')
             
 
             for event in pygame.event.get():
@@ -382,15 +422,13 @@ def gameLoop():
             clock.tick(300)
             
             pygame.display.update()
-                    
-            
 
-##                if event.type == pygame.KEYDOWN:
-##                    if event.key == pygame.K_q:
-##                        gameExit = True
-##                        gameOver = False
-##                    if event.key == pygame.K_c:
-##                        gameLoop()
+               # if event.type == pygame.KEYDOWN:
+               #     if event.key == pygame.K_q:
+               #         gameExit = True
+               #         gameOver = False
+               #     if event.key == pygame.K_c:
+               #         gameLoop()
 
 
         for event in pygame.event.get():
@@ -432,13 +470,12 @@ def gameLoop():
         lead_x += lead_x_change
         lead_y += lead_y_change
         
-##        gameDisplay.fill(white)
+        # gameDisplay.fill(white)
         gameDisplay.blit(bgd, (0,0))
         
-##        pygame.display.update()
+        # pygame.display.update()
 
-        
-##      pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, AppleThickness, AppleThickness])  
+        # pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, AppleThickness, AppleThickness])
 
         gameDisplay.blit(eba, (randAppleX, randAppleY))
         snakeHead = []
@@ -449,7 +486,7 @@ def gameLoop():
         if len(snakeList) > snakeLength:
                del snakeList[0]
 
-        #collision detection for loop:
+        # collision detection for loop:
         for eachSegment in snakeList[:-1]:
             if eachSegment == snakeHead:
                 snake_health = snake_health - 30
@@ -475,10 +512,10 @@ def gameLoop():
                snakeLength += 4
                score_value += 4
 
-##      The bonus handling code:
-##      bonus_text = medfont.render('Bonus:  +4', True, black)
+        # The bonus handling code:
+        # bonus_text = medfont.render('Bonus:  +4', True, black)
         if score_value == 17 or score_value == 49 or score_value == 81 or score_value == 113 or score_value == 145:
-##            gameDisplay.blit(bonus_text, [display_width/2 - 200, 100])            
+            # gameDisplay.blit(bonus_text, [display_width/2 - 200, 100])
             bonusAnim.play()
             bonusAnim.blit(gameDisplay, (display_width*0.35, display_height*0.3))
             pygame.display.update()
