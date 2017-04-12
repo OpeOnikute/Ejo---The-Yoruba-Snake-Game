88778da128ccd4f55c5e882a0bb671b35d884a0b
opeyemi.onikute <opeyemi.onikute@konga.com>
2017-04-12 08:37:29 +0100

    More code cleanup and fixed images import error

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 # -*- coding: cp1252 -*-
 import pygame
 import pyganim
-import time
+import os
+# import time
 import random
-import health
+# import health
 
 pygame.init()
 
+images_path = os.getcwd() + "\images\\"
+
 # Setting the display width and height
-display_width =  800
+display_width = 800
 display_height = 600
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Ejo')
 
 
 # colours
-white = (255,255,255)
-black = (0,0,0)
-grey = (220,220,220)
-red = (200, 0,0)
+white = (255, 255, 255)
+black = (0, 0, 0)
+grey = (220, 220, 220)
+red = (200, 0, 0)
 light_red = (255, 20, 20)
 green = (0, 140, 0)
 light_green = (0, 240, 0)
@@ -26,19 +29,19 @@ yellow = (255, 255, 128)
 light_yellow = (255, 255, 228)
 
 # loading the animations
-gameOverAnim = pyganim.PygAnimation([('game_over2.png', 150),('game_over3.png', 100)])
-bonusAnim = pyganim.PygAnimation([('bonus_text1.png', 150),('bonus_text2.png', 100)])
+gameOverAnim = pyganim.PygAnimation([(images_path+'game_over2.png', 150), (images_path+'game_over3.png', 100)])
+bonusAnim = pyganim.PygAnimation([(images_path+'bonus_text1.png', 150), (images_path+'bonus_text2.png', 100)])
 
 # loading the images
-icon = pygame.image.load('snakeHead.png')
-img = pygame.image.load('snake.png')
-eba = pygame.image.load('eba2.png')
-bgd = pygame.image.load('bgd.png')
-snakeBody = pygame.image.load('body.png')
-welcome_screen = pygame.image.load('welcome.png')
-game_over_face = pygame.image.load('face005.png')
-skin = pygame.image.load('skin.png')
-bgdside = pygame.image.load('bgd_side.png')
+icon = pygame.image.load(images_path+'snakeHead.png')
+img = pygame.image.load(images_path+'snake.png')
+eba = pygame.image.load(images_path+'eba2.png')
+bgd = pygame.image.load(images_path+'bgd.png')
+snakeBody = pygame.image.load(images_path+'body.png')
+welcome_screen = pygame.image.load(images_path+'welcome.png')
+game_over_face = pygame.image.load(images_path+'face005.png')
+skin = pygame.image.load(images_path+'skin.png')
+bgdside = pygame.image.load(images_path+'bgd_side.png')
 
 # setting the game icon to the snake head
 pygame.display.set_icon(icon)
@@ -51,38 +54,46 @@ FPS = 30
 direction = 'right'
 
 # Loading the fonts to be used
-extra_smallfont = pygame.font.SysFont('comicsansms', 15)
-smallfont = pygame.font.SysFont('comicsansms', 20)
-small2font = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 40)
-medfont = pygame.font.SysFont('comicsansms', 50)
-medfontButton = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 50)
-medfontButton2 = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 80)
-largefont = pygame.font.SysFont('comicsansms', 80)
-
+extra_smallfont = pygame.font.SysFont(r'comicsans', 15)
+smallfont = pygame.font.SysFont(r'comicsans', 20)
+# small2font = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 40)
+small2font = pygame.font.SysFont(r'comicsans', 40)
+medfont = pygame.font.SysFont(r'comicsans', 50)
+# medfontButton = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 50)
+medfontButton = pygame.font.SysFont(r'comicsans', 50)
+# medfontButton2 = pygame.font.Font(r'C:\Users\Ope O\Downloads\Fonts\eyelevation6.ttf', 80)
+medfontButton2 = pygame.font.SysFont(r'comicsans', 80)
+largefont = pygame.font.SysFont(r'comicsans', 80)
 # rendering the font for the copyright display at the bottom.
-sign = extra_smallfont.render('©2015.' , True, white)
-
-def message_to_screen(msg, color, y_displace=0, x_displace=0, size = "small", font = None, fontSize = None, blink=False):
-    textSurf, textRect = text_objects(msg, color, size, font, fontSize)
-    textRect.center = (display_width / 2)+x_displace, (display_height /2)+y_displace
-##    flashSurf = pygame.Surface((textSurf.get_width(), textSurf.get_height()))
-##    flashSurf = flashSurf.convert_alpha()
-
-    gameDisplay.blit(textSurf, textRect)
-##    origSurf = textSurf.copy()
+sign = extra_smallfont.render('©2017.', True, white)
 
 
-    
+def message_to_screen(msg, color, y_displace=0, x_displace=0, size="small", font=None, font_size=None):
+    """
+    :param msg:
+    :param color:
+    :param y_displace:
+    :param x_displace:
+    :param size:
+    :param font:
+    :param font_size:
+    :return:
+    """
+    text_surf, text_rect = text_objects(msg, color, size, font, font_size)
+    text_rect.center = (display_width / 2)+x_displace, (display_height /2)+y_displace
+    # flashSurf = pygame.Surface((textSurf.get_width(), textSurf.get_height()))
+    # flashSurf = flashSurf.convert_alpha()
+
+    gameDisplay.blit(text_surf, text_rect)
+    # origSurf = textSurf.copy()
+
+
 def pause():
 
     paused = True
-    message_to_screen('Paused',
-                        black,
-                      -100,
-                      size = 'large')
-    message_to_screen('Press C to continue or Q to quit.',
-                      black,
-                      25)
+    message_to_screen('Paused', black, -100, size='large')
+    message_to_screen('Press C to continue or Q to quit.', black, 25)
+
     while paused:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -113,7 +124,8 @@ def game_controls():
     """
     :return:
     """
-    gcont  = True
+
+    gcont = True
 
     while gcont:
         for event in pygame.event.get():
@@ -122,46 +134,33 @@ def game_controls():
                 quit()
                     
         gameDisplay.fill(white)
-##        message_to_screen(msg,
-##                          color,
-##                          y_displace=0,
-##                          x_displace=0,
-##                          size = "small", font = None, fontSize = None)
-        message_to_screen('CONTROLS',
-                           green,
-                           y_displace=-150,
-                           x_displace = 0, 
-                           size = 'large')
-        message_to_screen('Move Snake: Up, Down, Left and Right arrows',
-                          black,
-                          y_displace = 10,
-                          x_displace = 0,
-                          size = 'small')
-        message_to_screen('Pause: P ',
-                          black,
-                          y_displace = 50 ,
-                          x_displace = 0,
-                          size = 'small')
+        # message_to_screen(msg,
+        #                   color,
+        #                 y_displace=0,
+        #                   x_displace=0,
+        #                   size = "small", font = None, fontSize = None)
+        message_to_screen('CONTROLS', green, y_displace=-150, x_displace=0, size='large')
+        message_to_screen('Move Snake: Up, Down, Left and Right arrows', black, y_displace=10, x_displace=0, size='small')
+        message_to_screen('Pause: P ', black, y_displace=50, x_displace=0, size='small')
 
-        text_to_button('Play', yellow, light_yellow, 250,455,80,40, size = 'small', action = 'play')
-
+        text_to_button('Play', yellow, light_yellow, 250,455,80,40, size='small', action='play')
             
         # text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
         
-        text_to_button('Quit', red, light_red, 450,455,80,40 ,  size = 'small', action = 'quit')
+        text_to_button('Quit', red, light_red, 450, 455, 80, 40,  size='small', action='quit')
         pygame.display.update()
-
 
 
 def randAppleGen():
     """
     :return:
     """
-    randAppleX = round(random.randrange(40, display_width-AppleThickness-40))#/10)*10     #Eating with the Apple and regenerating a new apple somewhere else
-    randAppleY = round(random.randrange(30, display_height-AppleThickness-30))#/10)*10
+    randAppleX = round(random.randrange(40, display_width-AppleThickness-40))
+    randAppleY = round(random.randrange(30, display_height-AppleThickness-30))
 
     return randAppleX, randAppleY
- 
+
+
 def text_to_button(msg, color, inactive_color, buttonx, buttony, buttonwidth, buttonheight, size = None, action = None, blink = False):
     """
     :param msg:
@@ -184,11 +183,11 @@ def text_to_button(msg, color, inactive_color, buttonx, buttony, buttonwidth, bu
     cur = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     gameDisplay.blit(textSurf, (buttonx, buttony))
+
     if buttonx + textSize > cur[0] > buttonx and buttony + textSurf.get_height() > cur[1] > buttony:
         textSurf2, textRect2 = text_objects_button(msg, inactive_color, size)
         gameDisplay.blit(textSurf2, (buttonx-2, buttony-2))
-        
-        
+
         if click[0] == 1 and action != None:
             if action == 'quit':
                 pygame.quit()
@@ -199,12 +198,12 @@ def text_to_button(msg, color, inactive_color, buttonx, buttony, buttonwidth, bu
                 gameLoop()
             if action == 'main':
                 game_intro()
-
         # pygame.display.update()
 
 
 def game_intro():
-    intro  = True
+
+    intro = True
 
     while intro:
         for event in pygame.event.get():
@@ -217,8 +216,7 @@ def game_intro():
                 if event.key == pygame.K_q:
                     pygame.quit()
                     quit()
-                    
-        
+
         gameDisplay.fill(white)
 
         gameDisplay.blit(welcome_screen,  (0,0))
@@ -236,39 +234,46 @@ def game_intro():
         pygame.display.update()
         clock.tick(FPS)
 
-##def button(text, x, y, width, height, inactive_color, active_color, action = None):
-##    cur = pygame.mouse.get_pos()
-##    click = pygame.mouse.get_pressed()
-##    
-##    if x + width > cur[0] > x and y + height > cur[1] > y:
-##        pygame.draw.rect(gameDisplay, active_color, (x,y,width,height))
-##        pygame.draw.line(gameDisplay, black, (x, y),(x+width, y),3)
-##        pygame.draw.line(gameDisplay, black, (x+width, y),(x+width, y+height),3)
-##        pygame.draw.line(gameDisplay, black, (x+width, y+height),(x, y+height),3)
-##        pygame.draw.line(gameDisplay, black, (x, y+height),(x, y),3)
-##        
-##        if click[0] == 1 and action != None:
-##            if action == 'quit':
-##                pygame.quit()
-##                quit()
-##            if action == 'controls':
-##                game_controls()
-##            if action == 'play':
-##                gameLoop()
-##            if action == 'main':
-##                game_intro()
-##            
-##    else:
-##        pygame.draw.rect(gameDisplay, inactive_color, (x,y,width,height))
-##        pygame.draw.line(gameDisplay, black, (x, y),(x+width, y),3)
-##        pygame.draw.line(gameDisplay, black, (x+width, y),(x+width, y+height),3)
-##        pygame.draw.line(gameDisplay, black, (x+width, y+height),(x, y+height),3)
-##        pygame.draw.line(gameDisplay, black, (x, y+height),(x, y),3)
-##
-##    text_to_button(text, white, x,y,width,height)
+# def button(text, x, y, width, height, inactive_color, active_color, action = None):
+#    cur = pygame.mouse.get_pos()
+#    click = pygame.mouse.get_pressed()
+#
+#    if x + width > cur[0] > x and y + height > cur[1] > y:
+#        pygame.draw.rect(gameDisplay, active_color, (x,y,width,height))
+#        pygame.draw.line(gameDisplay, black, (x, y),(x+width, y),3)
+#        pygame.draw.line(gameDisplay, black, (x+width, y),(x+width, y+height),3)
+#        pygame.draw.line(gameDisplay, black, (x+width, y+height),(x, y+height),3)
+#        pygame.draw.line(gameDisplay, black, (x, y+height),(x, y),3)
+#
+#        if click[0] == 1 and action != None:
+#            if action == 'quit':
+#                pygame.quit()
+#                quit()
+#            if action == 'controls':
+#                game_controls()
+#            if action == 'play':
+#                gameLoop()
+#            if action == 'main':
+#                game_intro()
+#
+#    else:
+#        pygame.draw.rect(gameDisplay, inactive_color, (x,y,width,height))
+#        pygame.draw.line(gameDisplay, black, (x, y),(x+width, y),3)
+#        pygame.draw.line(gameDisplay, black, (x+width, y),(x+width, y+height),3)
+#        pygame.draw.line(gameDisplay, black, (x+width, y+height),(x, y+height),3)
+#        pygame.draw.line(gameDisplay, black, (x, y+height),(x, y),3)
+#
+#    text_to_button(text, white, x,y,width,height)
+
 
 def Snake(block_size, snakeList):
-    ##functionality for rotation:
+    """
+    functionality for rotation
+    :param block_size:
+    :param snakeList:
+    :return:
+    """
+
     if direction == 'right':
         head = pygame.transform.rotate(img, 270)
     if direction == 'left':
@@ -280,7 +285,7 @@ def Snake(block_size, snakeList):
     gameDisplay.blit(head,  (snakeList[-1][0], snakeList[-1][1]))
     for XnY in snakeList[:-1]:
         gameDisplay.blit(skin,  (XnY[0], XnY[1]))
-       # pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
+        # pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
     
 
 def text_objects(text, color, size = None,  font = None, fontSize = None):
@@ -367,8 +372,8 @@ def gameLoop():
     
     # The event handling loop is:
     while not gameExit:
-##        if gameOver == True:
-##            pygame.display.update()
+        # if gameOver == True:
+        #     pygame.display.update()
 
         while gameOver == True:
             gameOverAnim.play()
@@ -378,42 +383,39 @@ def gameLoop():
             gameDisplay.blit(bgdside, (0,0))
             gameDisplay.blit(sign, [display_width/2 - 30,display_height-20])
             health_bars(snake_health)
-           # pygame.time.set_timer(Text1, 10)
-           # pygame.time.set_timer(Text2, 90)
+            # pygame.time.set_timer(Text1, 10)
+            # pygame.time.set_timer(Text2, 90)
             
             
             gameOverAnim.blit(gameDisplay, (263, 352))
-##            message_to_screen("Game Over",
-##                              red,
-##                              y_displace=180,
-##                              x_displace = 25,
-##                              size = None,
-##                              font = 'BEARPAW_.ttf', 
-##                              fontSize = 90,
-##                               )
-            
-                
+            # message_to_screen("Game Over",
+            #                   red,
+            #                   y_displace=180,
+            #                   x_displace = 25,
+            #                   size = None,
+            #                   font = 'BEARPAW_.ttf',
+            #                   fontSize = 90,
+            #                    )
+
             message_to_screen('Score: ' + str((score_value-1)*2),
                               black,
-                              y_displace = -220,
+                              y_displace=-220,
                               x_displace=10, 
-                              size = None,
-                              font = 'VIDEOPHREAK.ttf',
-                              fontSize = 50)
+                              size=None,
+                              font='VIDEOPHREAK.ttf',
+                              font_size=50)
 
+            text_to_button('Play Again', black, light_yellow, 62, 455, 80, 40, size='small', action='play')
+            text_to_button('Play Again', yellow, light_yellow, 60, 455, 80, 40, size='small', action='play')
             
-            text_to_button('Play Again', black, light_yellow, 62,455,80,40, size = 'small', action = 'play')
-            text_to_button('Play Again', yellow, light_yellow, 60,455,80,40, size = 'small', action = 'play')
-            
-           # text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
-            text_to_button('Quit', black, black, 622,455,80,40 ,  size = 'small', action = 'quit')
-            text_to_button('Quit', red, light_red, 620,455,80,40 ,  size = 'small', action = 'quit')
+            # text_to_button('Quit', black, white, 612.5,457,80,40 ,  size = 'small', action = 'quit')
+            text_to_button('Quit', black, black, 622, 455, 80, 40,  size='small', action='quit')
+            text_to_button('Quit', red, light_red, 620, 455, 80, 40,  size='small', action='quit')
 
-           # button('Play Again', 150,400,100,40, black, light_green, action = 'play')
-           # button('Controls', 350,400,100,40, black, light_green, action = 'controls')
-           # button('Quit', 570,400,80,40 , black, light_green, action = 'quit')
+            # button('Play Again', 150,400,100,40, black, light_green, action = 'play')
+            # button('Controls', 350,400,100,40, black, light_green, action = 'controls')
+            # button('Quit', 570,400,80,40 , black, light_green, action = 'quit')
             
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameExit = True
@@ -423,13 +425,12 @@ def gameLoop():
             
             pygame.display.update()
 
-               # if event.type == pygame.KEYDOWN:
-               #     if event.key == pygame.K_q:
-               #         gameExit = True
-               #         gameOver = False
-               #     if event.key == pygame.K_c:
-               #         gameLoop()
-
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_q:
+            #         gameExit = True
+            #         gameOver = False
+            #     if event.key == pygame.K_c:
+            #         gameLoop()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -459,14 +460,11 @@ def gameLoop():
              pygame.display.update()
              gameOver = True
              
-            
-
             # Code to write if you want the stuff to stop moving when you relase a key
-            #if event.type == pygame.KEYUP:
+            # if event.type == pygame.KEYUP:
             #    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
             #        lead_x_change = 0
 
-        
         lead_x += lead_x_change
         lead_y += lead_y_change
         
@@ -502,7 +500,7 @@ def gameLoop():
         pygame.display.update()
 
         #And this is the code for when the snake 'eats' an apple
-        if lead_x > randAppleX and lead_x < randAppleX + AppleThickness or lead_x + block_size > randAppleX and lead_x + block_size < randAppleX + AppleThickness:
+        if (lead_x > randAppleX and lead_x < randAppleX + AppleThickness) or (lead_x + block_size > randAppleX and lead_x + block_size < randAppleX + AppleThickness):
             if lead_y > randAppleY and lead_y < randAppleY + AppleThickness:  
                 randAppleX, randAppleY = randAppleGen()
                 score_value += 4
